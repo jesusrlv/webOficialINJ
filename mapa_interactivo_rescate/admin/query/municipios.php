cb61e4a9b0881cb42eb20b399fdf7a753d06c4c7
jesusrlv <jesusrlvrojo@gmail.com>
2024-11-13 16:56:44 -0600

    db actualizada y front mas avanzado 3

--- mapa_interactivo_rescate/admin/query/municipios.php
+++ mapa_interactivo_rescate/admin/query/municipios.php
@@ -6,22 +6,36 @@
     if (!$resultadoSql) {
         die("Error en la consulta SQL: " . $conn->error);
     }
+
+    $sqlContar2 = "SELECT COUNT(*) AS total_espacios FROM espacio";
+      $resultadoContar2 = $conn->query($sqlContar2);
+      $rowContar2 = $resultadoContar2->fetch_assoc();
+      $contar2 = $rowContar2['total_espacios'];
+
     while($rowSQL = $resultadoSql->fetch_assoc()){
       $municipio = $rowSQL['id'];
 
       $sqlMunicipio = "SELECT * FROM espacio WHERE municipio = '$municipio'";
       $resultadoMunicipio = $conn->query($sqlMunicipio);
-      
+
       $sqlContar = "SELECT COUNT(*) AS total_espacios FROM espacio WHERE municipio = '$municipio'";
       $resultadoContar = $conn->query($sqlContar);
       $rowContar = $resultadoContar->fetch_assoc();
       $contar = $rowContar['total_espacios'];
+      
+      $promedio = ($contar * 100)/$contar2;
 
         echo'
         <h2 class="accordion-header">
             <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'.$rowSQL['id'].'" aria-expanded="true" aria-controls="collapse'.$rowSQL['id'].'">
+            
+            
               <span class="badge text-bg-primary text-end ms-3 ps-5 pe-5">'.$rowSQL['municipio'].'</span> <span class="badge text-bg-danger text-end ms-1 ps-3 pe-3">
 '.$contar.'</span>
+
+            <div class="progress ms-2" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+              <div class="progress-bar" style="width: '.$promedio.'%">'.$promedio.'</div>
+            </div>
             </button>
           </h2>';
           if ($rowSQL['id'] == 1){
