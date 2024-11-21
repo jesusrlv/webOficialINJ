90b8e1b9dd8a7908129a6a3aecafe9126dcafcce
jesusrlv <jesusrlvrojo@gmail.com>
2024-11-21 13:47:41 -0600

    localizacion front

--- mapa_interactivo_rescate/admin/query/queryTabla.php
+++ mapa_interactivo_rescate/admin/query/queryTabla.php
@@ -12,8 +12,8 @@
             <td>'.$x.'</td>
             <td>'.$row['nombre_espacio'].'</td>
             <td>'.$row['beneficiarios'].'</td>
-            <td>'.$row['fecha_intervencion'].'</td>
-            <td>'.$row['ubicacion'].'</td>
+            <td><span class="badge text-bg-secondary"><i class="bi bi-calendar-check-fill"></i> '.$row['fecha_intervencion'].'</span></td>
+            <td><a href="'.$row['ubicacion'].'" target="_blank"><span class="badge text-bg-warning"><i class="bi bi-geo"></i></span></a></td>
             <td><a href="#" onclick="modalAntes('.$row['id'].',1)"><span class="badge rounded-pill text-bg-danger"><i class="bi bi-arrow-left"></i></span></a></td>
             <td><a href="#" onclick="modalDespues('.$row['id'].',2)"><span class="badge rounded-pill text-bg-info"><i class="bi bi-arrow-right"></i></span></a>
 </td>
